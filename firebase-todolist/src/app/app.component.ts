161141f04bd6cddc84c700b26d0b551101713e23
Konrad Thiemann <konrad.gruss@t-online.de>
2023-06-02 14:53:49 +0200

    working todolist

--- firebase-todolist/src/app/app.component.ts
+++ firebase-todolist/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Firestore, collection, collectionData, doc, setDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebase-todolist';
+  todos$: Observable<any>;
+  todos:Array<any> = [];
+  todotext:string = '';
+
+  constructor(private firestore: Firestore) {
+    const coll = collection(firestore, 'todos');
+    this.todos$ = collectionData(coll);
+
+    this.todos$.subscribe((newTodos) => {
+      console.log('neue todos sind:', newTodos);
+      this.todos = newTodos;
+    });
+  }
+
+  addTodo(){
+    const coll = collection(this.firestore, 'todos');
+    setDoc(doc(coll), {name: this.todotext});
+  }
 }
