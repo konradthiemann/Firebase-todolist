8c8c079328880ece5e17517099beac13c5aee705
Konrad Thiemann <konrad.gruss@t-online.de>
2023-06-02 13:01:20 +0200

    first settings

---
+++ firebase-todolist/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'kt-firebase-todolist',
+    appId: '1:239537036652:web:76b88a1bd380f2062ee749',
+    storageBucket: 'kt-firebase-todolist.appspot.com',
+    apiKey: 'AIzaSyD5HHFiX0UzLQKSjPmsadu8FaMUfqbo3H8',
+    authDomain: 'kt-firebase-todolist.firebaseapp.com',
+    messagingSenderId: '239537036652',
+    measurementId: 'G-5P5RSWWP6N',
+  },
+};
